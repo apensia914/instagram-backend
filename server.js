8e861d98a54d8d194e89f850c9dd0ac39957f77b
Donghoon Han <apensia914@Donghoons-MacBook-Pro.local>
2021-04-12 20:58:03 +0900

    schema.prisma model created

--- server.js
+++ server.js
@@ -2,14 +2,35 @@ const { ApolloServer, gql } = require('apollo-server');
 
 // The GraphQL schema
 const typeDefs = gql`
+  type Movie {
+    id: Int
+    title: String
+    year: Int
+  }
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'bebe',
+    movies: () => [],
+    movie: () => ({ title: "Hello", year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (_, { title }) => {
+      console;log(title);
+      return true;
+    },
+    deleteMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
   },
 };
 
